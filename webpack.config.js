1c8d36555473a4158aa05049d585a4db13402ef6
scopp <osc.car2@gmail.com>
2022-11-11 20:30:34 +0100

    Add support to frontend typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,78 @@
 const path = require('path');
 const fs = require('fs');
 
-const filesConfiguration = [
-// POLYFILLS
-{
-	in: [
-		'core-js',
-		'./private/js/_dom/dom-modifications.js',
-		'regenerator-runtime/runtime',
-	],
-	out: "polyfill.min.js"
-},
-
-...fs.readdirSync('private/js', { withFileTypes:true })
-	.filter(element => element.isFile())
-	.map(file => file.name.replace(/\.js$/, ''))
-	.map(file => ({
-		in: `./private/js/${file}.js`,
-		out: `${file}.min.js`
-	}))
+const jsFilesConfiguration = [
+  // POLYFILLS
+  {
+    in: [
+      'core-js',
+      './private/js/_dom/dom-modifications.js',
+      'regenerator-runtime/runtime',
+    ],
+    out: "polyfill.min.js"
+  }
 ];
 
 
-module.exports = filesConfiguration.map(entry => (
-	{
-		entry: entry.in,
-		output: {
-			path: path.join(__dirname, 'public/js'),
-			publicPath: '/',
-			filename: entry.out
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								['@babel/preset-env', {
-									//"corejs": 3,
-									//"useBuiltIns": "usage"
-								}]
-							]
-						}
-					}
-				}
-			]
-		}
-	}
-));
+const tsFilesConfiguration = [
+  ...fs.readdirSync('private/js', { withFileTypes:true })
+    .filter(element => element.isFile())
+    .map(file => file.name.replace(/\.ts$/, ''))
+    .map(file => ({
+      in: `./private/js/${file}.ts`,
+      out: `${file}.min.js`
+    }))
+];
+
+module.exports = jsFilesConfiguration.map(entry => ({
+  entry: entry.in,
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/',
+    filename: entry.out
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}))
+.concat(
+  tsFilesConfiguration.map(entry => ({
+    entry: entry.in,
+    output: {
+      path: path.join(__dirname, 'public/js'),
+      publicPath: '/',
+      filename: entry.out,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[tj]sx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            },
+            'ts-loader',
+          ]
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  }))
+);
